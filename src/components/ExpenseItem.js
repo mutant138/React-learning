6ad446dabc117b21b76eb6ee70a7e287491e75ba
mutant138 <udhayasurya138@gmail.com>
2024-03-06 15:37:08 +0530

    js_varaibles_learnt

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,20 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2024, 2, 6);
+  const expenseTitle = "Car Insurance";
+  const expenseTitle1 = "Food";
+  const expenseAmount = 200;
   return (
-    <div className='expense-item'>
-      <div>Date</div>
-      <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>2500</div>
-        <h2>Food</h2>
-        <div className='expense-item__price'>250</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{expenseTitle1}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
         <h2>Movies</h2>
-        <div className='expense-item__price'>500</div>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
