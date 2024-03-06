51435cc7a614eb4365f1a1451d621041cd368bec
mutant138 <udhayasurya138@gmail.com>
2024-03-06 22:14:57 +0530

    learnt_dynamic_value_using_map

--- src/App.js
+++ src/App.js
@@ -2,10 +2,55 @@ import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title:'Car Insurance',
+      amount: 250,
+      date: new Date(2023,2,15)
+    },
+    {
+      id: 2,
+      title:'Car',
+      amount: 2500,
+      date: new Date(2023,3,15)
+    },
+    {
+      id: 3,
+      title:'Grocery',
+      amount: 2500,
+      date: new Date(2023,5,15)
+    },
+    {
+      id: 4,
+      title:'Bills',
+      amount: 5000,
+      date: new Date(2023,2,18)
+    },
+    {
+      id: 5,
+      title:'Car Insurance',
+      amount: 250,
+      date: new Date(2023,2,15)
+    },
+    {
+      id: 6,
+      title:'Car Insurance',
+      amount: 250,
+      date: new Date(2023,2,15)
+    },
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense, index) => (
+        <ExpenseItem
+          key={index}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2024, 2, 6);
-  const expenseTitle = "Car Insurance";
-  const expenseTitle1 = "Food";
-  const expenseAmount = 200;
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
-        <h2>{expenseTitle1}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
-        <h2>Movies</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
